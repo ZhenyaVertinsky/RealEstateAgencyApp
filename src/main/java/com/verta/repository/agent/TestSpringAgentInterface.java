be06351af960a9b0851158304c58ae4a81593eb6
Евгений <Zhenya.vertinsky@gmail.com>
2022-09-28 22:46:12 +0300

    AnnotationConfigApplicationContext

---
+++ src/main/java/com/verta/repository/agent/TestSpringAgentInterface.java
@@ -0,0 +1,53 @@
+package com.verta.repository.agent;
+
+import com.verta.domain.Agent;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+
+@Repository
+public class TestSpringAgentInterface implements AgentRepositoryInterface {
+
+    @Override
+    public Agent findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Optional<Agent> findOne(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Agent> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Agent> findAll(int limit, int offset) {
+        return null;
+    }
+
+    @Override
+    public Agent create(Agent object) {
+        return null;
+    }
+
+    @Override
+    public Agent update(Agent object) {
+        return null;
+    }
+
+    @Override
+    public Long delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAgentsStats() {
+        return null;
+    }
+}
