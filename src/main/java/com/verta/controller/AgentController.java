274da78fb9977383ff8799e2a1bb23f9ff3de4c6
Евгений <Zhenya.vertinsky@gmail.com>
2022-10-10 22:53:52 +0300

    Adjustment

--- src/main/java/com/verta/controller/AgentController.java
+++ src/main/java/com/verta/controller/AgentController.java
@@ -3,6 +3,7 @@ package com.verta.controller;
 import com.verta.domain.Agent;
 import com.verta.service.AgentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +12,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-
 @Controller
 @RequiredArgsConstructor
-
 public class AgentController {
 
     private final AgentService agentService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/agents")
-    public ModelAndView findAllAgent() {
+    public ModelAndView findAllAgents() {
         List<Agent> agents = agentService.findAll();
 
         ModelAndView model = new ModelAndView();
         model.addObject("agent", "Zhenya");
-        model.addObject("agent", agents);
+        model.addObject("agents", agents);
 
         model.setViewName("hello");
 
