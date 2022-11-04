599c35308041b8d2cef1367bb5de4a5bbde46a3b
Евгений <Zhenya.vertinsky@gmail.com>
2022-11-05 00:00:38 +0300

    Hibernate EntityManager Config (out SessionFactory)

--- src/main/java/com/verta/configuration/PersistenceProvidersConfiguration.java
+++ src/main/java/com/verta/configuration/PersistenceProvidersConfiguration.java
@@ -1,48 +1,52 @@
 package com.verta.configuration;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 public class PersistenceProvidersConfiguration {
 
-    @Bean(name = "sessionFactory")
-    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
-        // Fix Postgres JPA Error:
-        // Method org.postgresql.jdbc.PgConnection.createClob() is not yet implemented.
-        // properties.put("hibernate.temp.use_jdbc_metadata_defaults",false);
-
-        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-
-        // Package contain entity classes
-        factoryBean.setPackagesToScan("com.verta");
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setHibernateProperties(getAdditionalProperties());
-        factoryBean.afterPropertiesSet();
-        //
-        SessionFactory sf = factoryBean.getObject();
-        System.out.println("## getSessionFactory: " + sf);
-        return sf;
-    }
+//    @Bean(name = "sessionFactory")
+//    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+//        // Fix Postgres JPA Error:
+//        // Method org.postgresql.jdbc.PgConnection.createClob() is not yet implemented.
+//        // properties.put("hibernate.temp.use_jdbc_metadata_defaults",false);
+//
+//        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+//
+//        // Package contain entity classes
+//        factoryBean.setPackagesToScan("com.verta");
+//        factoryBean.setDataSource(dataSource);
+//        factoryBean.setHibernateProperties(getAdditionalProperties());
+//        factoryBean.afterPropertiesSet();
+//        //
+//        SessionFactory sf = factoryBean.getObject();
+//        System.out.println("## getSessionFactory: " + sf);
+//        return sf;
+//    }
 
     //Entity Manager
-//    @Autowired
-//    @Bean(name = "entityManagerFactory")
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-//        LocalContainerEntityManagerFactoryBean em
-//                = new LocalContainerEntityManagerFactoryBean();
-//        em.setDataSource(dataSource);
-//        em.setPackagesToScan("com.htp");
-//
-//        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//        em.setJpaVendorAdapter(vendorAdapter);
-//        em.setJpaProperties(getAdditionalProperties());
-//
-//        return em;
-//    }
+    @Autowired
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.verta");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(getAdditionalProperties());
+
+        return em;
+    }
 
     private Properties getAdditionalProperties() {
         Properties properties = new Properties();
