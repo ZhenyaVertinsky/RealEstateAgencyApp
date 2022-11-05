379af77af7b0975ed545c41df0d572f2f598fb4b
Евгений <Zhenya.vertinsky@gmail.com>
2022-11-05 20:18:34 +0300

    Spring Data methods and create linkage table row

--- src/main/java/com/verta/repository/springdata/AgentSpringDataRepository.java
+++ src/main/java/com/verta/repository/springdata/AgentSpringDataRepository.java
@@ -1,11 +1,47 @@
 package com.verta.repository.springdata;
 
+import com.verta.domain.Gender;
 import com.verta.domain.hibernate.HibernateAgent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface AgentSpringDataRepository extends CrudRepository<HibernateAgent, Long>,
-        JpaRepository<HibernateAgent, Long>, PagingAndSortingRepository<HibernateAgent, Long> {
+        JpaRepository<HibernateAgent, Long>, PagingAndSortingRepository<HibernateAgent, Long>{
+
+    HibernateAgent findByIdAndGender(Long id, Gender gender);
+
+    List<HibernateAgent> findByAgentLogin(String login);
+
+    List<HibernateAgent> findByAgentLoginAndAgentNameAndBirthday(String login, String name, Timestamp birthDate);
+
+    List<HibernateAgent> findByAgentLoginAndAgentNameOrBirthdayOrderByIdDescAgentNameDesc(String login, String name, Timestamp birthDate);
+
+    //select * from m_agents where (login = ? and name = ?) or birthday = ?
+
+    @Query(value = "select a from HibernateAgent a")
+    List<HibernateAgent> findByHQLQuery();
+
+    @Query(value = "select * from entity.agents", nativeQuery = true)
+    List<HibernateAgent> findByHQLQueryNative();
+
+    @Query(value = "select a from HibernateAgent a where a.agentLogin = :login and a.agentName = :agentName")
+    List<HibernateAgent> findByHQLQuery(String login, @Param("agentName") String name);
+
+    @Query("select a.id, a.agentName from HibernateAgent a")
+    List<Object[]> getPartsOfAgent();
+
+
+    @Modifying
+    @Query(value = "insert into entity.l_role_agent(agent_id, role_id) values (:agent_id, :role_id)", nativeQuery = true)
+    int createRoleRow(@Param("agent_id") Long agentId, @Param("role_id") Long roleId);
+
+
 
 }
