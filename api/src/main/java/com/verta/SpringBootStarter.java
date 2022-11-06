f2071894389c2865e31bca6b578890c62e43078e
Евгений <Zhenya.vertinsky@gmail.com>
2022-11-06 12:35:37 +0300

    Spring Cache

--- api/src/main/java/com/verta/SpringBootStarter.java
+++ api/src/main/java/com/verta/SpringBootStarter.java
@@ -4,6 +4,7 @@ package com.verta;
 import com.verta.configuration.PersistenceProvidersConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @Import({PersistenceProvidersConfiguration.class})
+@EnableCaching
 public class SpringBootStarter {
 
     public static void main(String[] args) {
